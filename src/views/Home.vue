154a6f8c67e5c010a6b71d6dd38297ab11e9f08f
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-05 23:38:41 +0530

    Text and Colors.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
+    <p
+      class="red blue--text"
+    >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto est laborum corrupti neque sapiente eos, ab itaque mollitia, veniam id voluptates tempore consectetur incidunt sunt numquam eveniet, odio explicabo! Dolorem.</p>
+    <p
+      class="pink lighten-4 red--text text--darken-4"
+    >Lorem, ipsum dolor sit amet consectetur adipisicing elit. Distinctio autem dignissimos fugit totam, eaque optio sunt beatae nulla maiores enim, asperiores aliquam cupiditate ipsam, ducimus ea voluptate quos repellat dolor!</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading">This is a sub heading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
