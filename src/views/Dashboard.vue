347a7882f832b92c106ffe76adffd1694fec0e46
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-06 23:27:47 +0530

    Project Data

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,30 +2,84 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     <v-container class="my-5">
-      <v-card flat class="pa-3">
-        <v-layout row wrap>
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
           <v-flex xs12 md6>
             <div class="caption grey--text">Project Title</div>
-            <div>Create a new website</div>
+            <div>{{ project.title }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Person</div>
-            <div>Sulay Sumaria</div>
+            <div>{{ project.person }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Due by</div>
-            <div>1st Jan 2019</div>
+            <div>{{ project.due }}</div>
           </v-flex>
           <v-flex xs6 sm4 md2>
             <div class="caption grey--text">Status</div>
-            <div>ongoing</div>
+            <div>{{ project.status }}</div>
           </v-flex>
         </v-layout>
+        <v-divider></v-divider>
       </v-card>
     </v-container>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      projects: [
+        {
+          title: "Design a new website",
+          person: "Sulay Sumaria",
+          due: "1st Jan 2019",
+          status: "ongoing",
+          content:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus suscipit iste iure, optio nulla esse, qui, quae ab consequatur eligendi aliquam rerum illum dignissimos similique minus. Dolorum cumque dignissimos eveniet?"
+        },
+        {
+          title: "Code up the homepage",
+          person: "Sulay Sumaria",
+          due: "10th Jan 2019",
+          status: "complete",
+          content:
+            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum reprehenderit laboriosam molestiae fuga vero voluptatem, recusandae, rem minus corrupti sunt pariatur explicabo inventore magnam maiores perspiciatis! Fugiat earum quam mollitia!"
+        },
+        {
+          title: "Design video thumbnails",
+          person: "Sulay Sumaria",
+          due: "20th Dec 2018",
+          status: "complete",
+          content:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, deserunt. Recusandae fugiat rem quas? Aperiam, praesentium perferendis voluptatem animi culpa ipsum atque mollitia dicta fugiat ipsa non sit itaque cumque!"
+        },
+        {
+          title: "Create a community forum",
+          person: "Sulay Sumaria",
+          due: "20th Oct 2018",
+          status: "overdue",
+          content:
+            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste repellat voluptatem asperiores reprehenderit accusantium quo natus cum earum pariatur quae omnis, ex rerum, a at qui excepturi? Expedita, minima ipsam."
+        }
+      ]
+    };
+  }
+};
 </script>
+
+<style scoped>
+.project.complete {
+  border-left: 4px solid #3cd1c2;
+}
+
+.project.ongoing {
+  border-left: 4px solid orange;
+}
+
+.project.overdue {
+  border-left: 4px solid tomato;
+}
+</style>
