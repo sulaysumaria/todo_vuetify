b99e0f0b10b3ebc3a41e92b5790b13af370f1ae2
Sulay Sumaria <sulaysumaria@gmail.com>
2019-05-06 08:27:49 +0530

    Toolbars

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="home">
     <h1>HomePage</h1>
-
-    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
-
-    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
-
-    <v-btn class="hidden-sm-only">click me</v-btn>
   </div>
 </template>
 
